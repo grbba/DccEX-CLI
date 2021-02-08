3f872e2365a159d343a02a3f53c5791b052e18c8
Gregor Baues <grbaues@gmail.com>
2021-02-08 11:37:36 +0100

    using cli complete example  but has uncaught exception error at runtime now

--- src/DccShell.cpp
+++ src/DccShell.cpp
@@ -21,94 +21,31 @@
 // #include "BufferedAsyncSerial.h"
 // #include <boost/asio.hpp>
 
+  #include <cli/boostasioscheduler.h>
+  #include <cli/boostasioremotecli.h>
+  namespace cli
+  {    
+      using MainScheduler = BoostAsioScheduler;
+      using CliTelnetServer = BoostAsioCliTelnetServer;
+  }
+
 #include "../include/cli/cli.h"
-#include "../include/cli/clifilesession.h"
+// #include "../include/cli/clifilesession.h"
+#include "../include/cli/clilocalsession.h"
+#include "../include/cli/filehistorystorage.h"
+
+#include <chrono>
 #include <fstream>
 
 #include "DccShell.hpp"
 #include "Diag.hpp"
 
-using namespace cli;
+using namespace std::this_thread;     // sleep_for, sleep_until
+using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+using std::chrono::system_clock;
 
-#define DCC_SERIAL 0
-#define DCC_ETHERNET 1
-#define DCC_CONN_UNKOWN 2
-
-bool shellExit = false;
-
-void dccReciever(boost::asio::serial_port *port);
-
-boost::asio::io_service io;
-boost::asio::serial_port serialPort(io);
-
-
-std::shared_ptr<std::thread>
-    _spDccSerialRead; //  = std::make_shared<std::thread>(dccReciever,
-                      //  &serialPort);
-
-// using namespace cli;
-
-void doSomething(const std::vector<std::string> arg) {
-  std::cout << "send: <";
-  size_t i = 0;
-  // int c = arg.size();
-  for (auto s : arg) {
-    std::cout << s.c_str(); // << " " << it->c_str();
-    if (i < arg.size() - 1) {
-      std::cout << "  ";
-      i++;
-    }
-  }
-  std::cout << ">\n";
-}
-
-void received(const char *data, unsigned int len) {
-  std::vector<char> v(data, data + len);
-  std::cout << "recieving ...\n";
-  for (unsigned int i = 0; i < v.size(); i++) {
-    if (v[i] == '\n') {
-      std::cout << std::endl;
-    } else {
-      if (v[i] < 32 || v[i] >= 0x7f)
-        std::cout.put(' '); // Remove non-ascii char
-      else
-        std::cout.put(v[i]);
-    }
-  }
-  std::cout.flush(); // Flush screen buffer
-}
-
-std::string readLine(boost::asio::serial_port *port) {
-  // Reading data char by char, code is optimized for simplicity, not speed
-  char c;
-  std::string result;
-  for (;;) {
-    boost::asio::read(*port, boost::asio::buffer(&c, 1));
-    switch (c) {
-    case '\r':
-      break;
-    case '\n':
-      return result;
-    default:
-      result += c;
-    }
-  }
-}
-
-void dccReciever(boost::asio::serial_port *port) {
-  INFO("\nreciever started : %d %x %d", port->is_open(), &shellExit, shellExit);
-  // std::cout << "\n" << port->is_open() << shellExit << '\n';
-  if (shellExit)
-    std::cout << "exit changed to true\n";
-  while (shellExit == false) {
-
-    if (shellExit)
-      std::cout << "exit changed to true\n";
-
-    std::cout << readLine(port) << '\n';
-  }
-  return;
-}
+boost::asio::io_service DccShell::io;
+boost::asio::serial_port DccShell::serial(io);
 
 // opens the connection on the serial port / connects over ethernet
 int openConnection(boost::asio::serial_port *sp, std::string type,
@@ -149,8 +86,7 @@ int openConnection(boost::asio::serial_port *sp, std::string type,
       std::cout << "\nConnected to " << port.c_str() << '\n';
       std::cout << rang::fg::reset;
 
-      // create the thread for reading - reads lines forever until closed
-      // std::thread dccSerialRead(dccReciever, baud);
+      // set the open flag for the reciever !
 
     } catch (const boost::system::system_error &e) {
       Diag::push();
@@ -175,78 +111,27 @@ int openConnection(boost::asio::serial_port *sp, std::string type,
   }
   return DCC_SUCCESS;
 }
-/**
- * @brief running the shell
- *
- * @return int
- */
-auto DccShell::runShell() -> int {
 
-  // SerialPort for connection
 
-  // thread to run the reciever
+//////////////////////
+// Building the Menus
+/////////////////////
 
-  std::cout << rang::style::italic
-            << "Interactive mode; Type 'help' for more information\n"
-            << rang::style::reset;
-
-  SetColor();
-
-  CmdHandler colorCmd;
-  CmdHandler nocolorCmd;
-
-  auto rootMenu = std::make_unique<Menu>("DccEX");
+// void DccShell::buildRootMenu(std::unique_ptr<cli::Menu> & rootMenu) {
+void DccShell::buildRootMenu(cli::Menu * rootMenu) {
 
   rootMenu->Insert(
       "config",
       [](std::ostream &) { std::cout << "Printing config" << std::endl; },
       "Shows the configuration items for the current session");
+}
 
-  rootMenu->Insert("file",
-                   [](std::ostream &out, int fd) {
-                     out << "file descriptor: " << fd << "\n";
-                   },
-                   "Print the file descriptor specified", {"file_descriptor"});
-  rootMenu->Insert(
-      "error", [](std::ostream &) { throw std::logic_error("Error in cmd"); },
-      "Throw an exception in the command handler");
+void DccShell::buildCsMenu(cli::Menu * csMenu) {
 
-  rootMenu->Insert(
-      "color",
-      [](std::ostream &out) {
-        out << "Colors ON\n";
-        SetColor();
-      },
-      "Enable colors in the cli");
-  rootMenu->Insert(
-      "nocolor",
-      [](std::ostream &out) {
-        out << "Colors OFF\n";
-        SetNoColor();
-      },
-      "Disable colors in the cli");
-  rootMenu->Insert("removecmds", [&](std::ostream &) {
-    colorCmd.Remove();
-    nocolorCmd.Remove();
-  });
-
-  // Menu for builing the layout and graph
-  auto buildMenu = std::make_unique<Menu>("build");
-
-  buildMenu->Insert(
-      "config", {"Shows the configuration items for the current session"},
+  csMenu->Insert(
+      "config",
       [](std::ostream &) { std::cout << "Printing config" << std::endl; },
       "Shows the configuration items for the current session");
-
-  buildMenu->Insert(
-      "load",
-      [](std::ostream &out, const std::string &arg) {
-        out << "Loading: " << arg << '\n';
-      },
-      "load <file> : loads a layout file");
-
-  // Menu for the command station
-  auto csMenu = std::make_unique<Menu>("cs");
 
   csMenu->Insert(
       "open", {"serial|ethernet", "serial port|ip address", "baud"},
@@ -254,9 +139,7 @@ auto DccShell::runShell() -> int {
          const int &baud) {
         int sfd;
         // ptr to the thread started here // the port is not yet open
-        if (openConnection(&serialPort, type, port, baud, &sfd)) {
-          _spDccSerialRead =
-              std::make_shared<std::thread>(dccReciever, &serialPort);
+        if (openConnection(&serial, type, port, baud, &sfd)) {
           out << rang::fg::green << "Success(" << sfd << ")\n"
               << rang::fg::reset;
         } else {
@@ -270,44 +153,23 @@ auto DccShell::runShell() -> int {
       "and "
       ", if not specified for serial, \n\t"
       "the default of 115200 will be used.");
+}
 
-  csMenu->Insert(
-      "send", {"list of DCC command parameters separated by space"},
-      [](std::ostream &out, std::vector<std::string> data) {
-        doSomething(data);
-        out << "Success\n";
-      },
-      "Send a < > command to the commandstation e.g.'send R 1 1' will send "
-      "<R "
-      "1 1> to the commandstation.");
+void DccShell::buildMenus() {
 
-  csMenu->Insert(
-      "bye", [](std::ostream &out) { out<< "set shellExit to true"; shellExit = true; },
-      "set shellExit to true");
+  auto rootMenu = std::make_unique<cli::Menu>("DccEX");
+  buildRootMenu(&*rootMenu);
 
-  //   csMenu->Insert(
-  //       "send",
-  //       [](std::ostream &out,const std::string arg) { doSomething(arg); out
-  //       << "Success\n"; },
-  //       // [](std::ostream &out, const std::vector<std::string>& args ...){
-  //       doSomething( args ); }, "Send a < > command to the
-  //       commandstation");
+  auto csMenu = std::make_unique<cli::Menu>("cs");
+  buildCsMenu(&*csMenu);
 
-  // csMenu->Insert(
-  //       "send", [](std::ostream &out, const std::string &arg) { out <<
-  //       "This is a sample!\n"; }, "Print a demo string");
-
-  rootMenu->Insert(std::move(buildMenu));
   rootMenu->Insert(std::move(csMenu));
 
-  Cli cli(std::move(rootMenu));
+  cli::Cli cli(std::move(rootMenu));
 
-  // global exit action
-  cli.ExitAction([](auto &out) {
-    out << "Goodbye and thanks for all the fish.\n";
-
-    _spDccSerialRead.get()->join();
-  });
+  // // global exit action
+  cli.ExitAction(
+      [](auto &out) { out << "Goodbye and thanks for all the fish.\n"; });
 
   // std exception custom handler
   cli.StdExceptionHandler(
@@ -316,61 +178,61 @@ auto DccShell::runShell() -> int {
             << " handling command: " << cmd << ".\n";
       });
 
-  CliFileSession input(cli);
-  _pCli = &cli;
-  _pInput = &input;
+  // cli::CliFileSession input(cli);
+  // input.Start();
 
-  // CliSession localSession(cli, scheduler, std::cout, 200);
-  input.ExitAction([](auto &out) // session exit action
-                   {
-                     out << "Closing App...\n";
-                     shellExit = true;
-                   });
+    cli::MainScheduler scheduler;
+    cli::CliLocalTerminalSession localSession(cli, scheduler, std::cout, 200);
+    localSession.ExitAction(
+        [&scheduler](auto& out) // session exit action
+        {
+            out << "Closing App...\n";
+            scheduler.Stop();
+        }
+    );
 
-  input.Start();
+    // setup server
 
-  return 1;
+    cli::CliTelnetServer server(scheduler, 5000, cli);
+    // exit action for all the connections
+    server.ExitAction( [](auto& out) { out << "Terminating this session...\n"; } );
+
+    scheduler.Run();
+
+
+  return;
 }
 
-// Cli cli(std::move(rootMenu));
+// function running in the reciever thread
+void dccReciever(bool *exit, boost::asio::serial_port *sp) {
+  std::cout << "inital exit signal " << *exit << '\n';
+  std::cout << "port is " << sp->is_open() << '\n';
+  // while ( sp->is_open() == false ) {};
+  //std::cout << "port is " << sp->is_open() << '\n';
 
-// cli.ExitAction([](auto &out) {
-//   shellExit = true;
-//   INFO("Goodbye and thanks for all the fish. %x %d", &shellExit, shellExit);
-//   // out << "Goodbye and thanks for all the fish.\n";
-// });
+  int l = 0;
+  while (*exit == false) {
+    l++; // std::cout << "read port\n";
+  }
+  std::cout << "thread exiting with " << l << " reads\n";
+}
 
-// // create a cli with the given root menu and a persistent storage
-// // you must pass to FileHistoryStorage the path of the history file
-// // if you don't pass the second argument, the cli will use a
-// // VolatileHistoryStorage object that keeps in memory the history of all the
-// // sessions, until the cli is shut down. Cli cli( std::move(rootMenu),
-// // std::make_unique<FileHistoryStorage>(".cli") ); global exit action
-// // cli.ExitAction( [](auto& out){ out << "Goodbye and thanks for all the
-// // fish.\n"; } ); std exception custom handler
-// cli.StdExceptionHandler(
-//     [](std::ostream &out, const std::string &cmd, const std::exception &e) {
-//   out << "Exception caught in cli handler: " << e.what()
-//       << " handling command: " << cmd << ".\n";
-//     });
+auto DccShell::runShell() -> int {
+  Diag::push();
+  Diag::setLogLevel(DiagLevel::LOGV_DEBUG);
 
-// MainScheduler scheduler;
-// CliLocalSession input(cli, scheduler, std::cout, 200);
+  // buildMenus();
 
-// input.ExitAction([](auto &out) // session exit action
-//                  {
-//   out << "Closing App...\n";
-//   scheduler.Stop();
-//   shellExit = true;
-//   _spDccSerialRead->join();
-//                  });
-
-// CliTelnetServer server(scheduler, 5000, cli);
-// // exit action for all the connections
-// server.ExitAction([](auto &out) {
-//   out << "Terminating this session...\n"; });
-
-// // input.Start();
-// scheduler.Run();
-// // _spDccSerialRead->join();
-// TRC("exiting: %d", shellExit);
+  DBG("Run Shell");
+  reciever = std::thread(dccReciever, &done, &serial);
+  DBG("Shell reciever thread started");
+  // simulate loop of the shell
+  // sleep_for(5ms);
+  buildMenus();
+  // once done set the exit flag to true
+  done = true;
+  reciever.join();
+  DBG("Shell done");
+  Diag::pop();
+  return DCC_SUCCESS;
+}
