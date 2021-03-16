cd5f632bf1577b0b53ac9cd9f5e174ca115cbe72
Gregor Baues <gr2bba@gmail.com>
2021-03-16 22:11:17 +0100

    added serial port list for windows

--- src/ShellCmdExec.cpp
+++ src/ShellCmdExec.cpp
@@ -424,7 +424,7 @@ void csPorts(std::ostream &out, std::shared_ptr<cmdItem> cmd, std::vector<std::s
     std::map<std::int8_t,std::string> ports;
     int8_t pn = 1;
     fmt::print("Available ports:\n");
-    
+
 #ifdef OS_MAC
     std::stringstream portList(exec("ls /dev/cu*"));
     std::string p;
@@ -461,6 +461,27 @@ void csPorts(std::ostream &out, std::shared_ptr<cmdItem> cmd, std::vector<std::s
     } catch (const std::filesystem::filesystem_error &ex) {
       ERR("{}", ex.what());
       throw ex;
+    }
+#endif
+#ifdef OS_WIN
+    char lpTargetPath[5000]; // buffer to store the path of the COMPORTS
+    bool gotPort = false; // in case the port is not found
+
+    for (int i = 0; i < 255; i++) // checking ports from COM0 to COM255
+    {
+        std::string str = "COM" + std::to_string(i); // converting to COM0, COM1, COM2
+        DWORD test = QueryDosDevice(str.c_str(), lpTargetPath, 5000);
+
+        // Test the return value and error if any
+        if (test != 0) //QueryDosDevice returns zero if it didn't find an object
+        {
+            std::cout << str << ": " << lpTargetPath << std::endl;
+            gotPort = true;
+        }
+
+        if (::GetLastError() == ERROR_INSUFFICIENT_BUFFER)
+        {
+        }
     }
 #endif
 }
